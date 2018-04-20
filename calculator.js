106a5fcc6ec8396dd579ab1b8a4cdbee054324b3
Mark Bungeroth <mbungeroth@gmail.com>
2018-04-19 20:25:06 -0400

    Keep necessary buttons. Adjust button colors and classes to match functionality. Add delete button

---
+++ calculator.js
@@ -0,0 +1,7 @@
+//when numbers are pressed, string version of num gets added to stringnum array
+//when non-num is pressed, turn num string into number, put number into final array
+//put operation in final array
+//when numbers are pressed, string version of num gets added to stringnum array
+//repeat
+
+//screen can display max 7 numbers including decimal
