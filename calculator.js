a91ea6a2af4a1d4b594a958ed68d9ce78517b791
Mark Bungeroth <mbungeroth@gmail.com>
2018-04-20 22:11:37 -0400

    give elements correct classes. add operations object and button ID object.

--- calculator.js
+++ calculator.js
@@ -5,3 +5,35 @@
 //repeat
 
 //screen can display max 7 numbers including decimal
+
+const operations = {
+  add: (num1, num2) => num1 + num2,
+  subtract: (num1, num2) => num1 - num2,
+  multiply: (num1, num2) => num1 * num2,
+  divide: (num1, num2) => num1 / num2
+};
+
+const buttons = {
+  zero: document.getElementsByClassName('zero'),
+  one: document.getElementsByClassName('one'),
+  two: document.getElementsByClassName('two'),
+  three: document.getElementsByClassName('three'),
+  four: document.getElementsByClassName('four'),
+  five: document.getElementsByClassName('five'),
+  six: document.getElementsByClassName('six'),
+  seven: document.getElementsByClassName('seven'),
+  eight: document.getElementsByClassName('eight'),
+  nine: document.getElementsByClassName('nine'),
+  clear: document.getElementsByClassName('clear'),
+  delete: document.getElementsByClassName('delete'),
+  negative: document.getElementsByClassName('negative'),
+  add: document.getElementsByClassName('add'),
+  subtract: document.getElementsByClassName('subtract'),
+  multiply: document.getElementsByClassName('multiply'),
+  divide: document.getElementsByClassName('divide'),
+  decimal: document.getElementsByClassName('decimal'),
+  equals: document.getElementsByClassName('equals')
+};
+
+const numsAndOperations = [];
+let temporaryNums = '';
